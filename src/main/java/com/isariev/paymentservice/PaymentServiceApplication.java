813ec2d15dafdc626f8e93a09a858fbf1b9c338f
Vladyslav <vladsw764@icloud.com>
2023-09-05 19:05:33 +0300

    enabled R2dbcAuditing

--- src/main/java/com/isariev/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/isariev/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.isariev.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 @SpringBootApplication
+@EnableR2dbcAuditing
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
