9b777a2e535d172ef373a1e8217ef4b7d0feb703
Vladyslav <vladsw764@icloud.com>
2023-09-06 15:22:30 +0300

    added methods into service

--- src/main/java/com/isariev/paymentservice/service/TransactionService.java
+++ src/main/java/com/isariev/paymentservice/service/TransactionService.java
@@ -2,8 +2,20 @@ package com.isariev.paymentservice.service;
 
 import com.isariev.paymentservice.dto.request.TransactionRequestDto;
 import com.isariev.paymentservice.dto.response.TransactionResponseDto;
+import com.isariev.paymentservice.model.Transaction;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 public interface TransactionService {
     Mono<TransactionResponseDto> createTransaction(TransactionRequestDto transactionRequest);
+
+    Mono<String> getTransactionStatus(UUID uid);
+
+    Mono<TransactionResponseDto> getById(UUID id, String requestURI, String method);
+
+    Flux<TransactionResponseDto> getAllTransactions(String requestURI, String method);
+
+    Mono<Void> receiveTransactionStatus(Transaction transaction);
 }
