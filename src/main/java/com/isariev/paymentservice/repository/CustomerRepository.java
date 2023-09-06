3db2c78cb365675838e0e56607e36643790bcd5f
Vladyslav <vladsw764@icloud.com>
2023-09-06 21:56:16 +0300

    added method 'findByEmail'

--- src/main/java/com/isariev/paymentservice/repository/CustomerRepository.java
+++ src/main/java/com/isariev/paymentservice/repository/CustomerRepository.java
@@ -3,9 +3,11 @@ package com.isariev.paymentservice.repository;
 import com.isariev.paymentservice.model.Customer;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 @Repository
 public interface CustomerRepository extends ReactiveCrudRepository<Customer, UUID> {
+    Mono<Customer> findByEmail(String email);
 }
