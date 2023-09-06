240038f04264189364c76046e02760ad145b7d0c
Vladyslav <vladsw764@icloud.com>
2023-09-06 15:18:31 +0300

    added endpoints for management transactions

--- src/main/java/com/isariev/paymentservice/controller/TransactionController.java
+++ src/main/java/com/isariev/paymentservice/controller/TransactionController.java
@@ -2,14 +2,16 @@ package com.isariev.paymentservice.controller;
 
 import com.isariev.paymentservice.dto.request.TransactionRequestDto;
 import com.isariev.paymentservice.dto.response.TransactionResponseDto;
+import com.isariev.paymentservice.model.Transaction;
 import com.isariev.paymentservice.service.TransactionService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/transactions")
@@ -24,5 +26,27 @@ public class TransactionController {
     @PostMapping
     public ResponseEntity<Mono<TransactionResponseDto>> createTransaction(@RequestBody TransactionRequestDto transactionRequest) {
         return new ResponseEntity<>(transactionService.createTransaction(transactionRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/status/{uid}")
+    public ResponseEntity<Mono<String>> getTransactionStatusById(@PathVariable(name = "uid") UUID uid) {
+        return ResponseEntity.ok(transactionService.getTransactionStatus(uid));
+    }
+
+    @PostMapping("/status")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Void> receiveTransactionStatus(@RequestBody Transaction transaction) {
+        return transactionService.receiveTransactionStatus(transaction);
+    }
+
+    @GetMapping
+    public ResponseEntity<Flux<TransactionResponseDto>> getAllTransactions(HttpServletRequest request) {
+        return ResponseEntity.ok(transactionService.getAllTransactions(request.getRequestURI(), request.getMethod()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Mono<TransactionResponseDto>> getTransaction(@PathVariable(name = "id") UUID uid,
+                                                                       HttpServletRequest request) {
+        return ResponseEntity.ok(transactionService.getById(uid, request.getRequestURI(), request.getMethod()));
     }
 }
