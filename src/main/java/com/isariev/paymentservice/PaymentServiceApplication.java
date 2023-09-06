97c1c3c1f96b2c5b18e4816ad75c17fb2a55adb1
Vladyslav <vladsw764@icloud.com>
2023-09-06 15:16:33 +0300

    enabled scheduling

--- src/main/java/com/isariev/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/isariev/paymentservice/PaymentServiceApplication.java
@@ -3,9 +3,11 @@ package com.isariev.paymentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableR2dbcAuditing
+@EnableScheduling
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
