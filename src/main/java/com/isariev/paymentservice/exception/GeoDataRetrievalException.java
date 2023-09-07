bac997ffa223f22bdad30d41c757154afc71f216
Vladyslav <vladsw764@icloud.com>
2023-09-07 15:27:31 +0300

    created 'GeoDataRetrievalException'

---
+++ src/main/java/com/isariev/paymentservice/exception/GeoDataRetrievalException.java
@@ -0,0 +1,7 @@
+package com.isariev.paymentservice.exception;
+
+public class GeoDataRetrievalException extends RuntimeException {
+    public GeoDataRetrievalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
