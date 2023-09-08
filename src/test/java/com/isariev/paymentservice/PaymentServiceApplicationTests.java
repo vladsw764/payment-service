e09acd51736319eaa8be6516a0f924be5d9da764
Vladyslav <vladsw764@icloud.com>
2023-09-08 09:13:10 +0300

    added test_container datasource properties

--- src/test/java/com/isariev/paymentservice/PaymentServiceApplicationTests.java
+++ src/test/java/com/isariev/paymentservice/PaymentServiceApplicationTests.java
@@ -1,6 +1,5 @@
 package com.isariev.paymentservice;
 
-import org.junit.ClassRule;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
