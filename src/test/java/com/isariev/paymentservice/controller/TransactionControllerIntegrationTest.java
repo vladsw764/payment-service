c5cbc4289e4af127f87b3e6b2582c092aa17072b
Vladyslav <vladsw764@icloud.com>
2023-09-08 09:13:34 +0300

    added order for test executions

--- src/test/java/com/isariev/paymentservice/controller/TransactionControllerIntegrationTest.java
+++ src/test/java/com/isariev/paymentservice/controller/TransactionControllerIntegrationTest.java
@@ -1,16 +1,19 @@
 package com.isariev.paymentservice.controller;
 
+import com.isariev.paymentservice.TestPaymentServiceApplication;
+import com.isariev.paymentservice.dto.payoutResponse.PayoutResponseDto;
 import com.isariev.paymentservice.dto.request.CustomerRequestDto;
 import com.isariev.paymentservice.dto.request.TransactionRequestDto;
 import com.isariev.paymentservice.dto.response.TransactionResponseDto;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,14 +21,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
-@TestPropertySource(locations = "classpath:application-test.yml")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TransactionControllerIntegrationTest {
+    private static final PostgreSQLContainer<?> postgresContainer = TestPaymentServiceApplication.postgresContainer();
 
     @Autowired
     private WebTestClient testClient;
+
     private TransactionRequestDto requestDto;
+
     @LocalServerPort
     private int port;
+
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", () -> "r2dbc:postgresql://localhost:7777/integration-tests-db");
+        registry.add("spring.r2dbc.username", () -> "postgres");
+        registry.add("spring.r2dbc.password", () -> "postgres");
+        registry.add("spring.flyway.url", () -> "jdbc:postgresql://localhost:7777/integration-tests-db");
+        registry.add("spring.flyway.username", () -> "postgres");
+        registry.add("spring.flyway.password", () -> "postgres");
+    }
+
+    @BeforeAll
+    static void startContainer() {
+        if (!postgresContainer.isRunning()) {
+            postgresContainer.start();
+        }
+    }
+
+    @AfterAll
+    static void stopContainer() {
+        postgresContainer.stop();
+    }
 
     @BeforeEach
     void setUp() {
@@ -46,6 +74,7 @@ class TransactionControllerIntegrationTest {
     }
 
     @Test
+    @Order(1)
     void createTransaction() {
         testClient.post()
                 .uri("/v1/transactions")
@@ -62,6 +91,7 @@ class TransactionControllerIntegrationTest {
     }
 
     @Test
+    @Order(2)
     void createPayout() {
         testClient.post()
                 .uri("/v1/transactions/payout")
@@ -78,6 +108,7 @@ class TransactionControllerIntegrationTest {
     }
 
     @Test
+    @Order(3)
     void getTransaction() {
         String transactionId = createTransactionAndGetId();
         testClient.get()
@@ -92,13 +123,14 @@ class TransactionControllerIntegrationTest {
     }
 
     @Test
+    @Order(4)
     void getPayoutTransaction() {
         String payoutTransactionId = createPayoutTransactionAndGetId();
         testClient.get()
                 .uri("/v1/transactions/payout/{id}", payoutTransactionId)
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(TransactionResponseDto.class)
+                .expectBody(PayoutResponseDto.class)
                 .value(responseDto -> {
                     assertThat(responseDto).isNotNull();
                     assertThat(responseDto.uid()).isEqualTo(payoutTransactionId);
@@ -106,6 +138,7 @@ class TransactionControllerIntegrationTest {
     }
 
     @Test
+    @Order(5)
     void getAllTransactions() {
         testClient.get()
                 .uri("/v1/transactions")
@@ -119,12 +152,13 @@ class TransactionControllerIntegrationTest {
     }
 
     @Test
+    @Order(6)
     void getAllPayoutTransactions() {
         testClient.get()
                 .uri("/v1/transactions/payout")
                 .exchange()
                 .expectStatus().isOk()
-                .expectBodyList(TransactionResponseDto.class)
+                .expectBodyList(PayoutResponseDto.class)
                 .value(responseDtoList -> {
                     assertThat(responseDtoList).isNotNull();
                     assertThat(responseDtoList).isNotEmpty();
@@ -148,12 +182,12 @@ class TransactionControllerIntegrationTest {
     }
 
     private String createPayoutTransactionAndGetId() {
-        TransactionResponseDto responseDto = testClient.post()
-                .uri("/v1/transactions")
+        PayoutResponseDto responseDto = testClient.post()
+                .uri("/v1/transactions/payout")
                 .bodyValue(requestDto)
                 .exchange()
                 .expectStatus().isCreated()
-                .expectBody(TransactionResponseDto.class)
+                .expectBody(PayoutResponseDto.class)
                 .returnResult()
                 .getResponseBody();
 
